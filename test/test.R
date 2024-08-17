36f0082aef6b7ab18827669abb8214a2560f305f
tsutomu-nagano <copyxshow@gmail.com>
2024-08-17 22:55:28 +0900

    テストコード追加

--- test/test.R
+++ test/test.R
@@ -8,6 +8,7 @@ library(readxl)
 library(openxlsx)
 library(readr)
 library(yaml)
+library(cli)
 
 
 xx <- list.files("R", full.names = TRUE, recursive = TRUE) %>%
@@ -18,6 +19,7 @@ purrr::map(function(src){
     }
 
 })
+
 
 
 test_that("ランダム文字列の作成",{
@@ -38,6 +40,26 @@ test_that("エラー率を設定した場合のコードリスト作成",{
     length <- 5
     actual <- sample_with_err(c("A","B","C"), 10, is_codelist = TRUE, usable_chars = LETTERS, err_rate = 0.2)
     expected <- c("Z","A","A","A","C","E","A","C","C","B")
+    testthat::expect_equal(expected, actual)
+
+})
+
+test_that("ファイル生成",{
+
+    set.seed(123)
+    src <- "./test/test.json"
+    actual.src <- "./test/test.csv"
+    expected.src <- "./test/test.csv"
+
+    dd <- DummyDataGen$new()
+    dd$setting_from_json(src)
+
+    rec <- 10
+    dd$generate(rec = rec, dest = actual.src)
+
+    expected <- readLines(expected.src)
+    actual <- readLines(actual.src)
+
     testthat::expect_equal(expected, actual)
 
 })
